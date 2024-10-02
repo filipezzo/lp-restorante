7d6811e765ea35b387ef1fa6bc3553def603df25
Filipe Avanzzo <avanzzodev@gmail.com>
2024-10-02 13:00:22 -0300

    feat: commit inicial do projeto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "app-green": "#10b981",
+      },
+      backgroundColor: {
+        "app-bg": "#141414",
       },
     },
   },
